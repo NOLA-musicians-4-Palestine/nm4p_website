726eaf510c67b9c71a4e7eb13db0bfa4abe27497
Simon Moushabeck <simonmoush@gmail.com>
2025-07-26 12:16:11 -0400

    try unnamed scopes

--- tools/test_service_account.py
+++ tools/test_service_account.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 
 creds = service_account.Credentials.from_service_account_file(
     "token.json",
-    scopes=["https://www.googleapis.com/auth/calendar.readonly"]
+    ["https://www.googleapis.com/auth/calendar.readonly"]
 )
 
 calendar_service = build("calendar", "v3", credentials=creds)
