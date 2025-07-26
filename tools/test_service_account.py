8b811f092aca58303fe2976e4fffab13d71af6ce
Simon Moushabeck <simonmoush@gmail.com>
2025-07-26 11:17:09 -0400

    no module google?
    I got an error on the first line of my code saying module google not
    found, which is strange because it should be running on the same image
    that the old file is running on. and the problem doesn't show up there
    does it? let's find out

--- tools/test_service_account.py
+++ tools/test_service_account.py
@@ -1,5 +1,5 @@
-from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 creds = service_account.Credentials.from_service_account_file(
     #"service-account-file.json",
