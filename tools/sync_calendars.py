0d2e65e40caec10bb912a6e5be5f1812473212cf
Simon Moushabeck <simonmoush@gmail.com>
2025-07-26 13:41:31 -0400

    oops

--- tools/sync_calendars.py
+++ tools/sync_calendars.py
@@ -33,7 +33,7 @@ def fetch_events(service):
         calendars = calendar_list.get("items", [])
 
         target_calendar_id = "0b3ffa27ffe7ad1f5e25331bfddc2f1b3352f7fad89712b24761041cdfa8fb3f@group.calendar.google.com"
-        events_result = calendar_service.events().list(calendarId=target_calendar_id).execute()
+        events_result = service.events().list(calendarId=target_calendar_id).execute()
         events = events_result.get("items", [])
 
         print("events way:")
