ce95348f05f90686c0e1c9243a2ded5266441738
Simon Moushabeck <simonmoush@gmail.com>
2025-07-26 11:27:36 -0400

    try with the same imports from sync_calendars.py
    maybe importing one of the previous things allows the google module to
    be loaded?!?!

--- tools/test_service_account.py
+++ tools/test_service_account.py
@@ -1,4 +1,16 @@
+import frontmatter
+import os
+import pytz
+import yaml
+from googleapiclient.http import MediaIoBaseDownload
+from slugify import slugify
+from datetime import datetime
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+# from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 creds = service_account.Credentials.from_service_account_file(
