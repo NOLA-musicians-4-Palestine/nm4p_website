cd3a7d3ee74683595f9ed1412f9cb60d9ef53d0e
Simon Moushabeck <simonmoush@gmail.com>
2025-07-27 20:10:49 -0400

    oops yet again

--- tools/sync_calendars.py
+++ tools/sync_calendars.py
@@ -122,7 +122,7 @@ def event_as_post(event, drive_service):
     post.metadata["date"] = f"{{ {event['start']} | date_to_string }}"
     post.metadata["ymd"] = read_event_date(event["start"]).strftime("%Y-%m-%d")
     post.metadata["time"] = f"{start_time} - {end_time}"
-    if "location" in post.metadata:
+    if "location" in event:
         post.metadata["location"] = event["location"]
     post.content = event.get("description", event["summary"])
 
